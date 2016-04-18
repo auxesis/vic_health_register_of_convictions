940934412b9d10f5a2d825c67724f2abe59e21b1
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-04-19 00:40:43 +1000

    Add very basic scraper, without any details for now

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,42 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'geokit'
+require 'pry'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+convictions = []
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Fetch the page
+agent = Mechanize.new
+page = agent.get("https://www2.health.vic.gov.au/public-health/food-safety/convictions-register")
+
+# Build up basic records
+elements = page.search('div.listing-container ol li')
+elements.each do |el|
+  conviction = {}
+  conviction['trading_name'] = el.at('h3').text.strip
+  party, address, council = el.search('div.content em').text.split(/\s*\|\s*/)
+  conviction['party_served'] = party
+  conviction['address'] = address
+  conviction['council'] = council
+  convictions << conviction
+end
+
+puts "Found #{convictions.size} convictions"
+
+# Geocode
+convictions.each do |conviction|
+  puts "Geocoding #{conviction["address"]}"
+  a = Geokit::Geocoders::GoogleGeocoder.geocode(conviction['address'])
+  location = {
+    'lat' => a.lat,
+    'lng' => a.lng,
+  }
+  conviction.merge!(location)
+end
+
+# Serialise
+convictions.each do |conviction|
+  ScraperWiki.save_sqlite(["address"], conviction)
+end
+
+puts "Done"
