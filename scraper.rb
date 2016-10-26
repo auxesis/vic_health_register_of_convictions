aae6cd481e8a42fffc174c9ff7eb8f5611bedc63
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-10-26 13:05:34 +1100

    Refactor scraper to be more debuggable, and not create duplicate data

--- scraper.rb
+++ scraper.rb
@@ -23,10 +23,19 @@ Geokit::Geocoders::GoogleGeocoder.api_key = ENV['MORPH_GOOGLE_API_KEY'] if ENV['
   'Court:' => 'court',
 }
 
-def fetch_detail(detail_url)
+def scrub(text)
+  text.gsub!(/[[:space:]]/, ' ') # convert all utf whitespace to simple space
+  text.strip
+end
+
+def get(url)
+  @agent ||= Mechanize.new
+  @agent.get(url)
+end
+
+def extract_detail(page)
   details = {}
 
-  page = @agent.get(detail_url)
   data_list = page.search('div#main div dl').first.children.map {|e| e.text? ? nil : e }.compact
   data_list.each_slice(2).with_index do |(key, value), index|
     dt = key.text
@@ -41,6 +50,55 @@ def fetch_detail(detail_url)
   return details
 end
 
+def extract_notices(page)
+  notices = []
+  page.search('div.contentInfo div.table-container tbody tr').each do |el|
+    notices << { 'link' => "#{base}#{el.search('a').first['href']}" }
+  end
+  notices
+end
+
+def build_conviction(conviction)
+  page    = get(conviction['link'])
+  details = extract_detail(page)
+  puts "Extracting #{details['address']}"
+
+  conviction.merge!(details)
+end
+
+def geocode(notice)
+  @addresses ||= {}
+
+  address = notice['address']
+
+  if @addresses[address]
+    puts "Geocoding [cache hit] #{address}"
+    location = @addresses[address]
+  else
+    puts "Geocoding #{address}"
+    a = Geokit::Geocoders::GoogleGeocoder.geocode(address)
+    location = {
+      'lat' => a.lat,
+      'lng' => a.lng,
+    }
+
+    @addresses[address] = location
+  end
+
+  notice.merge!(location)
+end
+
+def base
+  "https://www2.health.vic.gov.au/public-health/food-safety/convictions-register"
+end
+
+def existing_record_ids
+  return @cached if @cached
+  @cached = ScraperWiki.select('link from data').map {|r| r['link']}
+rescue SqliteMagic::NoSuchTable
+  []
+end
+
 def extract_basic_detail(el)
   conviction = {}
 
@@ -53,39 +111,39 @@ def extract_basic_detail(el)
   return conviction
 end
 
-convictions = []
+def extract_convictions(page)
+  convictions = []
 
-# Fetch the page
-@agent = Mechanize.new
-page = @agent.get("https://www2.health.vic.gov.au/public-health/food-safety/convictions-register")
+  # Build up basic records
+  elements = page.search('div.listing-container ol li')
+  elements.each do |el|
+    conviction = extract_basic_detail(el)
 
-# Build up basic records
-elements = page.search('div.listing-container ol li')
-elements.each do |el|
-  conviction = extract_basic_detail(el)
+    detail_url = el.search('a').first['href']
+    conviction.merge!({'link' => detail_url})
 
-  detail_url = el.search('a').first['href']
-  details    = fetch_detail(detail_url)
-  conviction.merge!(details)
-  conviction.merge!({'link' => detail_url})
+    convictions << conviction
+  end
 
-  convictions << conviction
+  convictions
 end
 
-puts "Found #{convictions.size} convictions"
 
-# Geocode
-convictions.each do |conviction|
-  puts "Geocoding #{conviction["address"]}"
-  a = Geokit::Geocoders::GoogleGeocoder.geocode(conviction['address'])
-  location = {
-    'lat' => a.lat,
-    'lng' => a.lng,
-  }
-  conviction.merge!(location)
+def main
+  page = get(base)
+
+  convictions = extract_convictions(page)
+  puts "### Found #{convictions.size} convictions"
+  new_convictions = convictions.select {|r| !existing_record_ids.include?(r['link']) }
+  puts "### There are #{new_convictions.size} new convictions"
+
+  new_convictions.map! {|c| build_conviction(c) }
+  new_convictions.map! {|c| geocode(c) }
+
+  # Serialise
+  ScraperWiki.save_sqlite(['link'], new_convictions)
+
+  puts "Done"
 end
 
-# Serialise
-ScraperWiki.save_sqlite(['conviction_number'], convictions)
-
-puts "Done"
+main()
